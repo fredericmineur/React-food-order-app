da3eb7d34150ebf48cd21a285d241f378ac0a1bc
Frederic Mineur <fredericmineur@gmail.com>
2021-10-28 16:01:55 +0100

    add state management for Cart and Modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,14 +11,14 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onCloseCart}>
       {cartItems}
       <div className={styles.total}>
         <span>Total amount</span>
         <span>35.62</span>
       </div>
       <div className={styles.actions}>
-          <button className={styles['button--alt']}>Close</button>
+          <button className={styles['button--alt']} onClick={props.onCloseCart}>Close</button>
           <button className={styles.button}>Order</button>
       </div>
     </Modal>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     return <Fragment>
         <header className={styles.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton>Cart</HeaderCartButton>
+            <HeaderCartButton onShowCart={props.onShowCart}/>
         </header>
         <div className={styles['main-image']}>
             <img src={mealsImage} alt="A table full of delicious food"/>
