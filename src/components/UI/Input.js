f132d0a6739a46590a74e771b7cbb9e8b1e4685e
Frederic Mineur <fredericmineur@gmail.com>
2021-10-29 23:18:28 +0100

    Forward refs and addItem setting

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import styles from './Input.module.css'
 
-const Input = props => {
+const Input = (props) => {
     return <div className={styles.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
-        <input  {...props.input}/>
+        <input ref={props.inputRef} {...props.input}/>
     </div>
 }
 
