dbc0f097609bef19baf44d46844aa7496bfee072
Frederic Mineur <fredericmineur@gmail.com>
2021-11-01 01:02:38 +0000

    reducer logic to remove item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,8 +10,12 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0 ;
 
-  const removeCartItemHandler = item => {};
-  const addCartItemHandler = id => {};
+  const removeCartItemHandler = id => {
+    cartCtx.removeItem(id);
+  };
+  const addCartItemHandler = item => {
+    cartCtx.addItem({...item, amount:1})
+  };
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
