7e2d2c89c5b7a430eccafbb6607aa403d3d27cdf
Frederic Mineur <fredericmineur@gmail.com>
2021-11-19 18:17:50 +0000

    fetch for GET meals request

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,71 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styles from './AvailableMeals.module.css';
 import Card from '../UI/Card.js';
 import MealItem from "./MealItem/MealItem";
+import { useState } from "react/cjs/react.development";
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: 'm1',
+//     name: 'Sushi',
+//     description: 'Finest fish and veggies',
+//     price: 22.99,
+//   },
+//   {
+//     id: 'm2',
+//     name: 'Schnitzel',
+//     description: 'A german specialty!',
+//     price: 16.5,
+//   },
+//   {
+//     id: 'm3',
+//     name: 'Barbecue Burger',
+//     description: 'American, raw, meaty',
+//     price: 12.99,
+//   },
+//   {
+//     id: 'm4',
+//     name: 'Green Bowl',
+//     description: 'Healthy...and green...',
+//     price: 18.99,
+//   },
+// ];
+
+
 
 const AvailableMeals = props =>{
 
-    const mealsList = DUMMY_MEALS.map((meal)=>{
+  const [meals, setMeals] = useState([]);
+
+  const fetchMealsHandler = async () => {
+    try {
+      const response = await fetch("https://react-food-order-app-f946a-default-rtdb.europe-west1.firebasedatabase.app/meals.json");
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
+      const loadedMeals = [];
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price
+        })
+      }
+      console.log(loadedMeals);
+      setMeals(loadedMeals);
+
+    } catch(error) {
+      console.log(error.message);
+    }
+  }
+
+  useEffect(()=>{
+    fetchMealsHandler();
+  }, []);
+
+    const mealsList = meals.map((meal)=>{
         return <MealItem 
         id={meal.id}
         key={meal.id} 
