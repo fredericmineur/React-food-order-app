dd7019ccef4a95bad39eb16ef0434919cee38739
Frederic Mineur <fredericmineur@gmail.com>
2021-11-22 21:59:43 +0000

    add user feedback after form submission

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, Fragment } from "react";
 import styles from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
@@ -7,6 +7,8 @@ import Checkout from "./Checkout";
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -22,20 +24,26 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
-  const submitOrderHandler = (userData) => {
-    fetch('https://react-food-order-app-f946a-default-rtdb.europe-west1.firebasedatabase.app/orders.json',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        userData: userData,
-        orderedItems: cartCtx.items
-      })
-    }
-)
-  }
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch(
+      "https://react-food-order-app-f946a-default-rtdb.europe-west1.firebasedatabase.app/orders.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userData: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+
+    setIsSubmitting(false);
+    setHasSubmitted(true);
+    cartCtx.clearCart();
+  };
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
@@ -67,17 +75,40 @@ const Cart = (props) => {
     </div>
   );
 
-  return (
-    <Modal onClose={props.onCloseCart}>
+  const cartModalContent = (
+    <Fragment>
       {cartItems}
       <div className={styles.total}>
         <span>Total amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onCloseCart} onSubmitOrder={submitOrderHandler}/>}
+      {isCheckout && (
+        <Checkout
+          onCancel={props.onCloseCart}
+          onSubmitOrder={submitOrderHandler}
+        />
+      )}
       {!isCheckout && modalActions}
-    </Modal>
+    </Fragment>
   );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+  const hasSubmittedModalContent = <Fragment>
+    <p>Your order has been submitted</p>
+    <div className={styles.actions}>
+      <button className={styles.button} onClick={props.onCloseCart}>
+        Close
+      </button>
+    </div>
+
+  </Fragment>
+
+
+  return <Modal onClose={props.onCloseCart}>
+    {!isSubmitting && !hasSubmitted && cartModalContent}
+    {isSubmitting && isSubmittingModalContent}
+    {hasSubmitted && hasSubmittedModalContent}
+    </Modal>;
 };
 
 export default Cart;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -60,7 +60,10 @@ const cartReducer = (state, action) => {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
+  }
 
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
   }
 
   return defaultCartState;
@@ -80,11 +83,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearItemsFromCartHandler = () => {
+    dispatchCartAction({type: "CLEAR"})
+  }
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearItemsFromCartHandler
   };
   return (
     <CartContext.Provider value={cartContext}>
